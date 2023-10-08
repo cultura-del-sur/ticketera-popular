572960012fbc1a23dda8fcf0ad8d97a2b4a79e52
Pablo Pissi <pablopissi@gmail.com>
2023-10-08 01:16:16 -0300

    Corro pre-commit en todos los archivos

--- api/ticket_manager/ticket_manager/settings/development.py
+++ api/ticket_manager/ticket_manager/settings/development.py
@@ -1,6 +1,6 @@
 from .base import *
 
-SECRET_KEY = 'django-insecure-_i^%ncwk^-14$c%t_o_w*1#2dh)9)t@6i&f&^obwx5r2bwi-63'
+SECRET_KEY = "django-insecure-_i^%ncwk^-14$c%t_o_w*1#2dh)9)t@6i&f&^obwx5r2bwi-63"
 DEBUG = True
 
 DATABASES = {
