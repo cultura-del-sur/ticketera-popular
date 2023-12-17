dc475a895d35bc253d0de3df17ace74c6b85de19
Nicolás Miñán <nm.imagenysonido@gmail.com>
2023-12-17 18:00:38 -0300

    front: agregar iconos en svg al header

--- frontend/src/shared/components/Header/Header.tsx
+++ frontend/src/shared/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import MenuIcon from 'shared/components/Header/MenuIcon'
 import Logo from 'shared/components/Logo'
 import { Column, Container, Row } from 'shared/grid/components'
 import c from './Header.module.scss'
+import { COLORS } from 'shared/config'
+import SearchIcon from 'shared/components/Header/SearchIcon'
 
 const Header = () => {
   return (
@@ -10,8 +12,8 @@ const Header = () => {
         <Column className={c.logoZone}><Logo /></Column>
         <Column className={c.menuZone}>
           <Row spacing={10}>
-            <Column>M</Column>
-            <Column><MenuIcon /></Column>
+            <Column><SearchIcon color={COLORS.secondary_0} /></Column>
+            <Column><MenuIcon color={COLORS.secondary_0} /></Column>
           </Row>
         </Column>
       </Row>
