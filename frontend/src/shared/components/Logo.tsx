e70815970ea292117d0de487cb6791d8385739f2
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-01-29 22:55:15 -0300

    upgrade frontend

--- frontend/src/shared/components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import { unbounded } from 'shared/fonts'
-import c from './Logo.module.scss'
-
-const Logo = () => {
-  return <div className={`${c.main} ${unbounded.className}`}><a>Ticketera<b>Popular</b></a></div>
-}
-
-export default Logo
