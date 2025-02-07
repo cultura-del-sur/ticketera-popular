e957a6949954cb63a5cd13e0c54ee3e1f4b3cd54
Juan Vacirca <vacirca.i@gmail.com>
2025-02-07 17:17:50 -0300

    Layout modified

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,13 +2,15 @@ import  Navbar  from "./components/nav-bar";
 import Ticket from './components/ticket';
 import Footer from "./components/footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from "./components/styles/Page.module.css"
+
 
  const Home = () => {
   return (
-    <> 
+    <>
     <Navbar />
+    <h1 className={styles.h1}>Bienvenido a ticketera popular</h1>
     <div>
-      <h1>Bienvenido a ticketera popular</h1>
       <Ticket title="Recital" buttonText="Reservar">
         <p>Proximamente</p>
       </Ticket>
