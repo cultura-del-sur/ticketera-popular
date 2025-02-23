d6d536b83d557fa92851e8a78e4fbd13575eb6b5
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-02-22 22:06:30 -0300

    backend: reorganize files

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "ticket_manager.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "ticket_manager.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
