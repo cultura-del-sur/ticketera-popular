594b0b1915a0fd8e6403009d6e0a0ffdefb96e93
Nicolás Miñán <nicom@limonrojo.com.ar>
2026-02-16 22:50:21 -0300

    Delete old frontend projects

--- frontend-v2/components/ui/SectionTitle.tsx
+++
@@ -1,7 +0,0 @@
-interface SectionTitleProps {
-  children: React.ReactNode;
-}
-
-export function SectionTitle({ children }: SectionTitleProps) {
-  return <h2 className="mb-12 text-3xl font-bold">{children}</h2>;
-}
